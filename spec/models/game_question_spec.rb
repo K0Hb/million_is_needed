25056969ba7a64c014bcdd6f84aa4762e8703171
Vjacheslav <goplit2010.konovalov@yandex.ru>
2022-09-06 13:16:52 +0300

    add test #add_friend_call

--- spec/models/game_question_spec.rb
+++ spec/models/game_question_spec.rb
@@ -77,5 +77,17 @@ RSpec.describe GameQuestion, type: :model do
         expect(ff.size).to eq 2
       end
     end
+
+    context '#add_friend_call' do
+      it 'help_hash has key :friend_call and help include text in locale' do
+        expect(game_question.help_hash).not_to include(:friend_call)
+
+        game_question.add_friend_call
+        expect(game_question.help_hash).to include(:friend_call)
+
+        fc = game_question.help_hash[:friend_call]
+        expect(fc).to include('вариант')
+      end
+    end
   end
 end
