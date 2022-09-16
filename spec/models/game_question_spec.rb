8c71a162c08a817975718a434bd95bfd7df869ae
Vjacheslav <goplit2010.konovalov@yandex.ru>
2022-09-16 10:14:21 +0300

    edit syntax error

--- spec/models/game_question_spec.rb
+++ spec/models/game_question_spec.rb
@@ -23,7 +23,7 @@ RSpec.describe GameQuestion, type: :model do
   end
 
   describe '#correct_answer_key' do
-    it 'reuturn correct key' do
+    it 'return correct key' do
       expect(game_question.correct_answer_key).to eq 'b'
     end
   end
